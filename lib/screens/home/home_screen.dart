64dfb9d3502d02f8989b99b0ece88574421e9fd7
Padmanabh Wattamwar <wattamwarp874@gmail.com>
2025-01-05 13:17:14 +0530

    code clean

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:elastic_run/extensions/containers.dart';
 import 'package:elastic_run/extensions/navigation.dart';
+import 'package:elastic_run/reusable_widgets/app_image.dart';
 import 'package:elastic_run/reusable_widgets/er_widgets.dart';
 import 'package:elastic_run/screens/add_customer/add_customer_screen.dart';
 import 'package:elastic_run/screens/return_data/sales_return_screen.dart';
@@ -15,7 +17,14 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            6.height,
+            const AppImage(
+              'https://www.elastic.run/images/svg/elasticrun-logo.svg',
+              height: 80,
+            ),
+            30.height,
             ErWidgets.filledButton(
                 text: 'Add Customers',
                 onPressed: () {
